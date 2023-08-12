d6eb72c7ab0b9f129d91755c36f56f504bb8ceca
Jason <30715413+Jason-S-Wu@users.noreply.github.com>
2023-08-11 20:58:42 -0400

    Hot Fix Roles Timeout

--- events/removeTimeouts.js
+++ events/removeTimeouts.js
@@ -4,32 +4,35 @@ const path = require('node:path');
 
 module.exports = {
   name: Events.GuildMemberUpdate,
-  async execute(member) {
+  async execute(member, new_member) {
     // if server is in the autoUntimeout list remove the timeout
-
     const autoUntimeoutPath = path.join(__dirname, '/../data/autoUntimeout.json');
     if (!fs.existsSync(autoUntimeoutPath)) {
       fs.writeFileSync(autoUntimeoutPath, JSON.stringify([]));
     }
+
     // read the autoUntimeout list
     const autoUntimeoutList = JSON.parse(fs.readFileSync(autoUntimeoutPath));
+
+    const unbanListPath = path.join(__dirname, '/../data/unbanList.json');
+    if (!fs.existsSync(unbanListPath)) {
+      fs.writeFileSync(unbanListPath, JSON.stringify([]));
+    }
+
+    // read the unban list
+    const unbanList = JSON.parse(fs.readFileSync(unbanListPath));
+
+    // if member is in the unban list add the roles back
+    const user = unbanList.find((entry) => entry.server === member.guild.id && entry.user === member.id);
+
+    if (user && user.roles.length != new_member.roles.cache.size - 1) {
+      await member.roles.add(user.roles, `User is in the unban list.`);
+    }
 
     // if server is in the autoUntimeout list remove the timeout
     const server = autoUntimeoutList.find((entry) => entry.server === member.guild.id);
     if (server && server.autoUntimeout) {
       member.timeout(null);
-      const unbanListPath = path.join(__dirname, '/../data/unbanList.json');
-      if (!fs.existsSync(unbanListPath)) {
-        fs.writeFileSync(unbanListPath, JSON.stringify([]));
-      }
-      // read the unban list
-      const unbanList = JSON.parse(fs.readFileSync(unbanListPath));
-
-      // if member is in the unban list add the roles back
-      const user = unbanList.find((entry) => entry.server === member.guild.id && entry.user === member.id);
-      if (user) {
-        await member.roles.add(user.roles, `User is in the unban list.`);
-      }
     }
   },
 };
