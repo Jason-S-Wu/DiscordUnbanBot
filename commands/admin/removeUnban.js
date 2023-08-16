d98033a267277eeda02c1ae7ee5ad2695f4129e5
Jason <30715413+Jason-S-Wu@users.noreply.github.com>
2023-08-15 21:52:36 -0400

    hot fix unban

--- commands/admin/removeUnban.js
+++ commands/admin/removeUnban.js
@@ -18,7 +18,7 @@ module.exports = {
   async execute(interaction) {
     const server = interaction.guild;
     const user = interaction.options.getUser('user');
-    const alias = interaction.options.getString('alias').toLowerCase();
+    const alias = interaction.options.getString('alias');
 
     /*
         The unban list is a JSON file that looks like this:
